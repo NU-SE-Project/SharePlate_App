0d8b335f190ab9aeab3f3caddbdf77e730cbb11c
skajanan2002 <IT23335332@my.sliit.lk>
2026-02-22 00:07:47 +0530

    pickup time otp verify

--- backend/routes/pickupRoutes.js
+++ backend/routes/pickupRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createPickupOTP,
+  verifyPickupOTP,
 } from "../controllers/pickupController.js";
 
 const router = express.Router();
@@ -8,5 +9,7 @@ const router = express.Router();
 // Generate OTP 
 router.post("/generate", createPickupOTP);
 
+// Verify OTP (Restaurant enters OTP)
+router.post("/verify", verifyPickupOTP);
 
 export default router;
